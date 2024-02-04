f7f41ee2cd8758fcaa557fd0826a7638404721c7
johnmark-wd <john@support.webartdesign.com.au>
2024-02-05 03:58:27 +0800

    Activity Lab 1

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 3000;
+const EmployeeModel = require('./Employee');
+
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/ITD103', {})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    EmployeeModel.find()
+        .then(users => res.json(users))
+        .catch(err => res.json(err));
+});
+
+app.get('/get/:id', (req, res) => {
+    const id = req.params.id;
+    EmployeeModel.findById({ _id: id })
+        .then(employee => res.json(employee))
+        .catch(err => res.json(err));
+});
+
+app.post('/create', (req, res) => {
+    EmployeeModel.create(req.body)
+        .then(employee => res.json({ message: 'Employee created successfully' }, employee))
+        .catch(err => res.json(err));
+});
+
+app.put('/update/:id', (req, res) => {
+    const id = req.params.id;
+    EmployeeModel.findByIdAndUpdate(id, req.body, { new: true }) 
+        .then(employee => res.json({ message: 'Employee updated successfully' }, employee))
+        .catch(err => res.json(err));
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    EmployeeModel.findByIdAndDelete(id) 
+        .then(() => res.json({ message: 'Employee deleted successfully' }))
+        .catch(err => res.json(err));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
